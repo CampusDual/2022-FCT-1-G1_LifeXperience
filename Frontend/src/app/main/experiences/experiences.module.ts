ea3a4df2c399d99f3dd65686e9276ab5fecfa3cc
JorgeIgMar <jorgeimsistemas@gmail.com>
2022-05-13 13:26:48 +0200

    Creado módulo experiencias

---
+++ Frontend/src/app/main/experiences/experiences.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ExperiencesRoutingModule } from './experiences-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ExperiencesRoutingModule
+  ]
+})
+export class ExperiencesModule { }
