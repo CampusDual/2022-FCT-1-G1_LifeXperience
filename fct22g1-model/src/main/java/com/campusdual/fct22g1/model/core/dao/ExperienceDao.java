fc3702c0bd29f1641f45c3a1bcabedb1082330f2
Mario Cascudo <mariocscdclase@gmail.com>
2022-05-13 11:23:41 +0200

    Arreglando errores en los archivos xml producidos por el merge anterior

--- fct22g1-model/src/main/java/com/campusdual/fct22g1/model/core/dao/ExperienceDao.java
+++ fct22g1-model/src/main/java/com/campusdual/fct22g1/model/core/dao/ExperienceDao.java
@@ -1,4 +1,5 @@
 package com.campusdual.fct22g1.model.core.dao;
+
 import com.ontimize.jee.server.dao.common.ConfigurationFile;
 import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
 import org.springframework.context.annotation.Lazy;
@@ -9,11 +10,10 @@ import org.springframework.stereotype.Repository;
 @ConfigurationFile(configurationFile = "dao/ExperienceDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
 public class ExperienceDao extends OntimizeJdbcDaoSupport {
 
- public static final String ATTR_ID = "id"; 
- public static final String ATTR_NAME = "name";
- public static final String ATTR_DESCRIPTION = "description"; 
- public static final String ATTR_STARTDATE = "startdate";
- public static final String ATTR_ENDDATE = "enddate";
- 
+    public static final String ATTR_ID = "id";
+    public static final String ATTR_NAME = "name";
+    public static final String ATTR_DESCRIPTION = "description";
+    public static final String ATTR_STARTDATE = "startdate";
+    public static final String ATTR_ENDDATE = "enddate";
 
 }
