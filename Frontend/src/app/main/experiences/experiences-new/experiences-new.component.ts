d9b63bfc798a73d9006e02191b1aced659ae0389
Feяnando <62938201+fgonsua77@users.noreply.github.com>
2022-05-18 10:08:41 +0200

    formulario de crear experiencias

---
+++ Frontend/src/app/main/experiences/experiences-new/experiences-new.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-experiences-new',
+  templateUrl: './experiences-new.component.html',
+  styleUrls: ['./experiences-new.component.css']
+})
+export class ExperiencesNewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- Frontend/src/app/main/experiences/experiences.module.ts
+++ Frontend/src/app/main/experiences/experiences.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { ExperiencesRoutingModule } from './experiences-routing.module';
 import { ExperiencesHomeComponent } from './experiences-home/experiences-home.component';
+import { ExperiencesNewComponent } from './experiences-new/experiences-new.component';
 
 
 @NgModule({
-  declarations: [ExperiencesHomeComponent],
+  declarations: [ExperiencesHomeComponent, ExperiencesNewComponent],
   imports: [
     CommonModule,
     OntimizeWebModule,
