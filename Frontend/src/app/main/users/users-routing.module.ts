01d0276dafbc7b8a38c51c3e0df6f636a3413e9a
Mario Cascudo <mariocscdclase@gmail.com>
2022-05-18 18:05:55 +0200

    Hecho parcialmente el formulario para la creacion de usuarios, falta, por lo menos, mejorarlo estéticamente.

--- Frontend/src/app/main/users/users-routing.module.ts
+++ Frontend/src/app/main/users/users-routing.module.ts
@@ -3,14 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { UsersHomeComponent } from './users-home/users-home.component';
 import { UsersNewComponent } from './users-new/users-new.component';
 
-
+//TODO:Cambiar para que vuelva a redirigir a home
 const routes: Routes = [
-  {
+  /*{
     path: '',
     component : UsersHomeComponent
-  },
+  },*/
   {
-  path : 'new',
+  path : /*'new'*/'',
   component :UsersNewComponent
 }];
 
