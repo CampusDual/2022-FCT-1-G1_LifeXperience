3e4d1c4dd653c4c31fefec74f60a25ff31281917
Mario Cascudo <mariocscdclase@gmail.com>
2022-05-18 14:02:36 +0200

    Creada una rama para implementar un formulario para crear usuarios

--- Frontend/src/app/main/main-routing.module.ts
+++ Frontend/src/app/main/main-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuardService } from 'ontimize-web-ngx';
-
 import { MainComponent } from './main.component';
 
 export const routes: Routes = [
@@ -13,7 +12,8 @@ export const routes: Routes = [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
       { path: 'subscriptions', loadChildren: () => import('./subscriptions/subscriptions.module').then(m => m.SubscriptionsModule) },
-      { path: 'experiences', loadChildren: () => import('./experiences/experiences.module').then(m => m.ExperiencesModule) }
+      { path: 'experiences', loadChildren: () => import('./experiences/experiences.module').then(m => m.ExperiencesModule) },
+      { path: 'users', loadChildren:() => import('./users/users.module').then(m=> m.UsersModule)}
     ]
   }
 ];
