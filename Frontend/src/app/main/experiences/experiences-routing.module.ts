35c6b43dd2037b66558e0fe9d1c17e1d0c9a87f2
JorgeIgMar <jorgeimsistemas@gmail.com>
2022-05-18 12:55:38 +0200

    Creado formulario nueva experiencia

--- Frontend/src/app/main/experiences/experiences-routing.module.ts
+++ Frontend/src/app/main/experiences/experiences-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ExperiencesHomeComponent } from './experiences-home/experiences-home.component';
-
+import { ExperiencesNewComponent } from "./experiences-new/experiences-new.component";
 
 const routes: Routes = [{
   path : '',
   component: ExperiencesHomeComponent
-}];
+},
+{
+  path: "new",
+  component: ExperiencesNewComponent
+},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
