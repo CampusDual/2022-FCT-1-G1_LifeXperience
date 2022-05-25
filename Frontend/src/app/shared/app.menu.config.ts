34bb804c0f541b849dfdf00f1ac4e53d25353205
JorgeIgMar <jorgeimsistemas@gmail.com>
2022-05-25 17:24:33 +0200

    Primera iteración del anidamiento de menú lateral, código comentado por posible futura actualización

--- Frontend/src/app/shared/app.menu.config.ts
+++ Frontend/src/app/shared/app.menu.config.ts
@@ -6,8 +6,22 @@ import {
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
   { id: 'users', name: 'USERS', icon: 'people', route: '/main/users' },
-  { id: 'packages', name: 'PACKAGES', icon: 'paid', route: '/main/packages' },
-  {id: 'clients', name: 'CLIENTS', icon: 'people', route: '/main/clients'},
-  { id: 'experiences', name: 'EXPERIENCES', icon: 'travel_explore', route: '/main/experiences' },
+  {
+    id: 'modules', name: 'MODULES', icon: 'remove_red_eye', opened: false,
+    items: [
+
+           /*  { id: 'packagesAll', name: 'PACKAGES', icon: 'paid',opened:false, items:[
+              {
+                 id: 'packages', name: 'LIST', icon:'paid', route: '/main/packages'
+              },
+              {
+                id: 'new-package', name: 'NEW', icon:'paid', route: '/main/packages/new'
+              }
+
+            ]}, */
+            { id: 'packages', name: 'PACKAGES', icon: 'paid', route:'/main/packages'},
+            { id: 'clients', name: 'CLIENTS', icon: 'people', route: '/main/clients'},
+            { id: 'experiences', name: 'EXPERIENCES', icon: 'travel_explore', route: '/main/experiences'}]
+  },
   { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
 ];
