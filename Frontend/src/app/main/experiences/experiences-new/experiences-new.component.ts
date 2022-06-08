427360af7f7f92af7001629fb0365dc4b113888c
JorgeIgMar <jorgeimsistemas@gmail.com>
2022-06-08 12:43:05 +0200

    G1-91 Mapa experiencias en New y Details completo

--- Frontend/src/app/main/experiences/experiences-new/experiences-new.component.ts
+++ Frontend/src/app/main/experiences/experiences-new/experiences-new.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ElementRef, NgZone } from '@angular/core';
+import { MapsAPILoader, MouseEvent } from '@agm/core';
 @Component({
   selector: 'app-experiences-new',
   templateUrl: './experiences-new.component.html',
@@ -7,9 +7,84 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperiencesNewComponent implements OnInit {
 
-  constructor() { }
+  title: string = 'AGM project';
+  latitude!: number;
+  longitude!: number;
+  address: string;
+  private geoCoder;
+
+  @ViewChild('search', { static: false })
+  public searchElementRef!: ElementRef;
+
+
+  constructor(
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone
+  ) { }
+
 
   ngOnInit() {
-  }
+    //load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      this.setCurrentLocation();
+      this.geoCoder = new google.maps.Geocoder;
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement);
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
 
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+          //set latitude and longitude
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+        });
+      });
+    });
+  }
+  private setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.getAddress(this.latitude, this.longitude);
+      });
+    }
+  }
+  markerDragEnd($event: MouseEvent) {
+    console.log($event);
+    this.latitude = $event.coords.lat;
+    this.longitude = $event.coords.lng;
+    this.getAddress(this.latitude, this.longitude);
+  }
+  getAddress(latitude, longitude) {
+    this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results, status) => {
+      console.log(results);
+      console.log(status);
+      if (status === 'OK') {
+        if (results[0]) {
+
+          this.address = results[0].formatted_address;
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+
+    });
+  }
+  onMapClicked(event: any) {
+    console.table(event.coords);
+    this.latitude = event.coords.lat;
+    this.longitude = event.coords.lng;
+    this.getAddress(this.latitude, this.longitude)
+
+    }
 }
