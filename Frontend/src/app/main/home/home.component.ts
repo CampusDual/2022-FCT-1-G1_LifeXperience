a2eef8584781f173bd5e691b6a7648d138c96cc3
mario <mario@MARIOCASCUDO.local>
2022-06-14 11:22:28 +0200

    Creado grafico donde se muestrta los pagos de experiencias por dias, esto es una base  para pulir y empezar el resto

--- Frontend/src/app/main/home/home.component.ts
+++ Frontend/src/app/main/home/home.component.ts
@@ -1,26 +1,35 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { CandlestickChartConfiguration, DataAdapterUtils, LineChartConfiguration, OChartComponent, PieChartConfiguration } from 'ontimize-web-ngx-charts';
+import { Component, Injector, OnInit, ViewChild } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { OntimizeService, SQLTypes } from "ontimize-web-ngx";
+import {
+  CandlestickChartConfiguration,
+  DataAdapterUtils,
+  LineChartConfiguration,
+  OChartComponent,
+  PieChartConfiguration,
+} from "ontimize-web-ngx-charts";
 
 @Component({
-  selector: 'home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
-  
+  protected service: OntimizeService;
+  protected graphData: Array<Object>;
+
   constructor(
     private router: Router,
     private actRoute: ActivatedRoute,
-  ) {
-  }
+    private injector: Injector
+  ) {}
 
   ngOnInit() {
     // nothing to do
   }
 
   navigate() {
-    this.router.navigate(['../', 'login'], { relativeTo: this.actRoute });
+    this.router.navigate(["../", "login"], { relativeTo: this.actRoute });
   }
 
   goToDocumentation() {
@@ -32,38 +41,44 @@ export class HomeComponent implements OnInit {
   chartParameters: LineChartConfiguration;
 
   ngAfterViewInit() {
-    let data = [
-      { "date": 1511111115707, "open": 145.11, "high": 146.15, "low": 144.73, "close": 146.06, },
-      { "date": 1511111115708, "open": 145.99, "high": 146.37, "low": 145.34, "close": 145.73 },
-      { "date": 1511111115709, "open": 145.97, "high": 146.61, "low": 145.67, "close": 146.37 },
-      { "date": 1511111115712, "open": 145.85, "high": 146.11, "low": 145.43, "close": 145.97 },
-      { "date": 1511111115713, "open": 145.71, "high": 145.91, "low": 144.98, "close": 145.55 },
-      { "date": 1511111115714, "open": 145.87, "high": 146.32, "low": 145.64, "close": 145.92 },
-      { "date": 1511111115715, "open": 146.73, "high": 147.09, "low": 145.97, "close": 147.08 },
-      { "date": 1511111115716, "open": 147.04, "high": 147.15, "low": 146.61, "close": 147.07 },
-      { "date": 1511111115719, "open": 146.89, "high": 147.07, "low": 146.43, "close": 146.97 },
-      { "date": 1511111115720, "open": 146.29, "high": 147.21, "low": 146.2, "close": 147.07 },
-      { "date": 1511111115721, "open": 146.77, "high": 147.28, "low": 146.61, "close": 147.05 },
-      { "date": 1511111115722, "open": 147.7, "high": 148.42, "low": 147.15, "close": 148 },
-      { "date": 1511111115723, "open": 147.97, "high": 148.49, "low": 147.43, "close": 148.33 },
-      { "date": 1511111115727, "open": 148.33, "high": 149.13, "low": 147.98, "close": 149.1 },
-      { "date": 1511111115728, "open": 149.13, "high": 149.5, "low": 148.86, "close": 149.37 },
-      { "date": 1511111115729, "open": 149.15, "high": 150.14, "low": 149.01, "close": 149.41 }
-    ];
-
     this.chartParameters = new LineChartConfiguration();
 
-
-
-      this.chartParameters.xAxis='date';
-      this.chartParameters.yAxis =['open','close','low'];
-      this.chartParameters.isArea = [true];
-      this.chartParameters.interactive = false;
-      this.chartParameters.useInteractiveGuideline = false;
+    this.chartParameters.xAxis = "paymentdate";
+    this.chartParameters.xDataType = "time";
+    this.chartParameters.yAxis = ["amountpaid"];
+    this.chartParameters.isArea = [true];
+    this.chartParameters.interactive = false;
+    this.chartParameters.useInteractiveGuideline = false;
 
     DataAdapterUtils.createDataAdapter(this.chartParameters);
-    this.candlestick.setDataArray(DataAdapterUtils.adapter.adaptResult(data));
-  }
-  
 
+    this.getExpPayments();
+  }
+
+  getExpPayments() {
+    this.service = this.injector.get(OntimizeService);
+    const conf = this.service.getDefaultServiceConfiguration("experiences");
+    this.service.configureService(conf);
+
+    this.service
+      .query(void 0, ["amountpaid", "paymentdate"], "clientExperienceDetails")
+      .subscribe((resp) => {
+        if (resp.code === 0) {
+          this.candlestick.setDataArray(
+            DataAdapterUtils.adapter.adaptResult(resp.data)
+          );
+        } else {
+          console.log("Error");
+        }
+      });
+  }
+
+  adaptResult(data: any) {
+    if (data && data.length) {
+      data.forEach((item: any, index: number) => {
+        var a = item;
+        console.log(a);
+      });
+    }
+  }
 }
