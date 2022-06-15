f1b38857426bcfdf03988e9d31a735dd4916c9b7
Feяnando <62938201+fgonsua77@users.noreply.github.com>
2022-06-15 10:27:03 +0200

    G1-100 Finiquitada

--- Frontend/src/app/main/clients/clients-new/clients-new.component.ts
+++ Frontend/src/app/main/clients/clients-new/clients-new.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ValidatorFn, ValidationErrors, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-clients-new',
   templateUrl: './clients-new.component.html',
   styleUrls: ['./clients-new.component.css']
 })
 export class ClientsNewComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
   }
+
+
+
+   ageValidator(control: FormControl): ValidationErrors {
+      let result = {};
+      let currentdateYear = new Date().getFullYear();
+      let selectedDateYear = new Date(control.value).getFullYear();
+      if (control.value && (currentdateYear - selectedDateYear < 18)) {
+        result['adult'] = true;
+      }
+      return result;
+    }
 
 }
