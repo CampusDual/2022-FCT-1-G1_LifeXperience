6cc0f8a4cc971dcc6f40bb3ef92ef293a1a889de
Mario Cascudo <mariocscdclase@gmail.com>
2022-06-22 09:47:15 +0200

    Añadida generación dinamica de tarjetas para mostrar detalles. Conectado el portal con el backend

--- Frontend/src/app/portal-web/portal-web.component.ts
+++ Frontend/src/app/portal-web/portal-web.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, Injector, OnInit, ViewEncapsulation } from '@angular/core';
+import { OntimizeService } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'portal-web',
@@ -7,7 +8,38 @@ import { Component, Inject, Injector, OnInit, ViewEncapsulation } from '@angular
   encapsulation: ViewEncapsulation.None
 })
 export class PortalWebComponent implements OnInit {
+  private service: OntimizeService;
+  private experienceList;
+
   ngOnInit(): void {
     console.log("Constructor del nonPortalUser");
+    this.getExperiences();
   }
+
+  constructor(
+    private injector: Injector,
+  ){}
+
+  getExperiences() {
+    this.service = this.injector.get(OntimizeService);
+    const conf = this.service.getDefaultServiceConfiguration("portalService");
+    this.service.configureService(conf);
+
+    const columns = ['associate_image','name','description'];
+
+    this.service.query(null, columns, 'experience').subscribe(resp => {
+      if (resp.code === 0) {
+        this.experienceList = resp.data
+      } else {
+        alert('Impossible to query data!');
+        throw new Error
+      }
+    });
+  }
+
+
+  prepareImg(base64Img:string){
+    return "data:image/png;base64," + base64Img
+  }
+
 }
